9e3b762495babe835e863bf287515e9012ce32bd
ouyangyinong <118651909+ouyangyinong@users.noreply.github.com>
2022-11-20 17:18:36 +0800

    Add files via upload

---
+++ 数据分类.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+data = pd.read_csv('预处理.csv',engine='python')
+df1 = data.groupby(['大类编码','大类名称','中类编码','中类名称','小类编码','小类名称']).sum()
+df1.to_csv('分类1.csv',encoding='utf-8-sig')
