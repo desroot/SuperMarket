4b256612c8101e9480c8648484c1cd119394ab77
ouyangyinong <118651909+ouyangyinong@users.noreply.github.com>
2022-11-22 14:58:59 +0800

    Update 数据分类.py

--- 数据分类.py
+++ 数据分类.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import numpy as np
-data = pd.read_csv('预处理.csv',engine='python')
-df1 = data.groupby(['大类编码','大类名称','中类编码','中类名称','小类编码','小类名称']).sum()
-df1.to_csv('超市分类数据1.0.csv',encoding='utf-8-sig')
+data = pd.read_csv('数据预处理2.csv',encoding='gbk')
+df1 = data.groupby(['大类编码','大类名称','中类编码','中类名称','小类编码','小类名称']).mean()
+df1.to_csv('超市数据分类2.csv',encoding='gbk')
